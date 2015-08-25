8cfaeb505deb8022a754691a72d939df0a0768c4
Aaron Schiff <aaron@schiff.co.nz>
2015-08-26 08:28:24 +1200

    minor improvements to comments

--- roadcode.R
+++ roadcode.R
@@ -10,14 +10,14 @@ mapYRange = roads@bbox["y", "max"] - roads@bbox["y", "min"]
 aspect = mapYRange / mapXRange
 mapWidth = 1000         # Width of each individual map in pixels
 numMapsPerRow = 3       # Number of maps to plot in a row
-numRows = 2              # Number of rows of maps
+numRows = 2             # Number of rows of maps
 mapHeight = round(aspect * mapWidth)
 roadWidth = 0.8
 png("nz-roads.png", width = numMapsPerRow * mapWidth, height = numRows * mapHeight)
 par(mfrow = c(numRows, numMapsPerRow), mar = c(0, 0, 0, 0), bg = "#f8f8f8", family = "Fira Sans")
 
 # Parameters for labels
-labelOffset = 0.05    # Ratio to x and y range
+labelOffset = 0.05    # Ratio to x and y range, ie relative position from top-left corner
 labelSize = 6
 
 # Sealed roads
